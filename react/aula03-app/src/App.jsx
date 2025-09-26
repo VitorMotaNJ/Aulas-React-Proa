a1028346a733c2ecac9e16f08ee82319062d5d81
vitor-mota <vitormotanovais@gmail.com>
2025-09-26 12:40:56 -0300

    Adicionando componente da camada 4 da pratica da aula 3 de  react

--- react/aula03-app/src/App.jsx
+++ react/aula03-app/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 
 import Header from './components/Header'
 import Navigation from './components/Navigation'
+import Content from './components/Content'
 
 import './App.css'
 
@@ -12,6 +13,7 @@ function App() {
     <main className='principal'>
       <Header />
       <Navigation />
+      <Content />
     </main>
   )
 }
---
+++ react/aula03-app/src/components/Content.jsx
@@ -0,0 +1,11 @@
+import styles from './Content.module.css'
+
+function Content(){
+    return(
+        <section className={styles.content}>
+            Conteudo da camada 4
+        </section>
+    )
+}
+
+export default Content
