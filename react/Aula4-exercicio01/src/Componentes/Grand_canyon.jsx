473521623e9875349f59a1139f8d32260aa253ea
vitor-mota <vitormotanovais@gmail.com>
2025-10-06 10:09:45 -0300

    Adicionando o componente Grand_canyon junto da sua estilização

---
+++ react/Aula4-exercicio01/src/Componentes/Grand_canyon.jsx
@@ -0,0 +1,11 @@
+import Styles from './css/Grand_canyon.module.css'
+
+function Grand_canyon(){
+    return(
+       <section className={Styles.fundo_grand_canyon}>
+            <h1>Venha conhecer o Grand Canyon!</h1>
+       </section>
+    )
+}
+
+export default Grand_canyon
