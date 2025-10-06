49a3c165328876e579a6cbad0250914af457b043
vitor-mota <vitormotanovais@gmail.com>
2025-10-06 10:10:59 -0300

    Adicionando importançõesa dos novos componentes ao App.jsx para testar se está tundo funcionando

--- react/Aula4-exercicio01/src/App.jsx
+++ react/Aula4-exercicio01/src/App.jsx
@@ -3,6 +3,10 @@ import './App.css'
 import Header from './Componentes/Header'
 import Footer from './Componentes/Footer'
 import Home from './Componentes/Home'
+import Escocia from './Componentes/Escocia'
+import Aruba from './Componentes/Aruba'
+import Muralha_da_china from './Componentes/Muralha_da_china'
+import Grand_canyon from './Componentes/Grand_canyon'
 
 function App() {
   return (
@@ -10,6 +14,10 @@ function App() {
       <main>
         <Header />
         <Home />
+        <Aruba/>
+        <Muralha_da_china/>
+        <Grand_canyon/>
+        <Escocia />
         <Footer />
       </main>
     </>
