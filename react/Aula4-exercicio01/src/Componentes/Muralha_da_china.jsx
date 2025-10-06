37a7474299d068704f644cf62cf566500b0163ed
vitor-mota <vitormotanovais@gmail.com>
2025-10-06 10:10:30 -0300

    Adicionando componente Muralha_da_china junto da sua estilização

---
+++ react/Aula4-exercicio01/src/Componentes/Muralha_da_china.jsx
@@ -0,0 +1,11 @@
+import Styles from './css/Muralha_da_china.module.css'
+
+function Muralha_da_china(){
+    return(
+       <section className={Styles.fundo_muralha_da_china}>
+            <h1>Venha conhecer a Muralha da china!</h1>
+       </section>
+    )
+}
+
+export default Muralha_da_china
