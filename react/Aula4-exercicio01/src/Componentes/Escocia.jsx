6b6d930f4558c7d7ee64b8e1b2bfcc7f2b1cc715
vitor-mota <vitormotanovais@gmail.com>
2025-10-06 10:06:43 -0300

    Adicionando componente Escocia e sua estilização

---
+++ react/Aula4-exercicio01/src/Componentes/Escocia.jsx
@@ -0,0 +1,11 @@
+import Styles from './css/Escocia.module.css'
+
+function Escocia(){
+    return(
+       <section className={Styles.fundo_escocia}>
+            <h1>Venha conhecer a Escócia!</h1>
+       </section>
+    )
+}
+
+export default Escocia
