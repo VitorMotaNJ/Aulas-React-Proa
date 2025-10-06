8d0755568342afe946dc3b56775c97a6670a8c30
vitor-mota <vitormotanovais@gmail.com>
2025-10-06 10:08:28 -0300

    Adicionando componente Aruba

---
+++ react/Aula4-exercicio01/src/Componentes/Aruba.jsx
@@ -0,0 +1,11 @@
+import Styles from './css/Aruba.module.css'
+
+function Aruba(){
+    return(
+       <section className={Styles.fundo_aruba}>
+            <h1>Venha conhecer Aruba!</h1>
+       </section>
+    )
+}
+
+export default Aruba
